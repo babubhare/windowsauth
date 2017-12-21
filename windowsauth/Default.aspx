6ecac26aa98aa4c6a464b34608b79d7892d86ff6
Babu Chandranna <babu.chandranna@aptean.com>
2017-12-21 17:25:55 +0530

    Add project files.

---
+++ windowsauth/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ page title="Home Page" language="C#" masterpagefile="~/Site.Master" autoeventwireup="true" codebehind="Default.aspx.cs" inherits="windowsauth._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <div class="row">
+
+        <div class="col-md-4">
+            <h2>Logged in user: </h2>
+            <h2>
+                <asp:Label ID="loggedInUser" runat="server"></asp:Label></h2>
+        </div>
+
+        <div class="col-md-6">
+            <h2>Connection String:</h2>
+            <p>
+                Data Source=localhost;Initial Catalog=WTAuthTest;Integrated Security=SSPI;
+            </p>
+            <p>
+                <asp:TextBox ID="connectionString" Text="Data Source=localhost;Initial Catalog=WTAuthTest;Integrated Security=SSPI;" 
+                    runat="server" Width="470px"></asp:TextBox>
+            </p>
+        </div>
+
+        <div class="col-md-6">
+            <h2>Results:</h2>
+            <p>
+                Count from Table1
+                <asp:Button Text="Connect" runat="server" ID="connect" OnClick="connect_Click" />
+            </p>
+            <p>
+                <asp:TextBox ID="results" Text="" 
+                    runat="server" Width="470px" Height="137px" TextMode="MultiLine"></asp:TextBox>
+            </p>
+        </div>
+
+    </div>
+
+</asp:Content>
---
+++ windowsauth/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace windowsauth
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            IIdentity winid = HttpContext.Current.User.Identity;
+            loggedInUser.Text = winid.Name;
+
+        }
+
+        protected void connect_Click(object sender, EventArgs e)
+        {
+            results.Text = "Connecting .....";
+            var connectionStr = connectionString.Text;
+            var query = "SELECT COUNT(*) AS TOTAL FROM TABLE1";
+            using (SqlConnection connection =
+            new SqlConnection(connectionStr))
+            {
+                SqlCommand command = new SqlCommand(query, connection);
+                try
+                {
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        results.Text = reader[0].ToString();
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    results.Text = (ex.ToString());
+                }
+            }
+        }
+    }
+}
